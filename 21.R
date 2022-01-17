f751f5dacad11f059daac325c1d53cd565e4b8c4
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2022-01-17 20:51:32 +0530

    R programming

---
+++ 21.R
@@ -0,0 +1,12 @@
+x ="mobile"
+x
+x="pen"
+x
+x='makeup'
+x
+# data types in r
+#Number
+num1 = 10
+num2 = 20
+class(num1)
+class(num2)
---
+++ R_ln_pg1.r
@@ -0,0 +1,46 @@
+
+
+> x
+Error: object 'x' not found
+> x ="mobile"
+> x
+[1] "mobile"
+> x
+[1] "mobile"
+> x="pen"
+> x
+[1] "pen"
+> x='makeup'
+> x
+[1] "makeup"
+> x
+[1] "makeup"
+> class(num2)
+Error: object 'num2' not found
+> # data types in r
+> #Number
+> num1 = 10
+> num2 =20
+> class(num1)
+[1] "numeric"
+> class(num2)
+[1] "numeric"
+> char1="monkey"
+> class(char1)
+[1] "character"
+> log1=TRUE
+> class(log1)
+[1] "logical"
+> complex1=5+7i
+> class(complex1)
+[1] "complex"
+> rm(complex1)
+> complex1
+Error: object 'complex1' not found
+> nchar(char1)
+[1] 6
+> x<-c(1,2,3,4,5,6,7,8,9,10)
+> for i in x:
+Error: unexpected symbol in "for i"
+> x*8
+ [1]  8 16 24 32 40 48 56 64 72 80
